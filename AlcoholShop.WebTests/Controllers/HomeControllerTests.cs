32448ab2d76d2a4b73f789da5d8a45a56d499832
Georgi Hristov <g.hristov@ds-bg.com>
2017-05-03 17:42:50 +0300

    Some changes...

---
+++ AlcoholShop.WebTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlcoholShop.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
